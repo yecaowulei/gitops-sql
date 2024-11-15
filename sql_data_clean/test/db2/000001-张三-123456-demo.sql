845cdb730cd2534ea90bd048435e663ec4d26d42
yecaowulei <nanfei.li@3dxt.com>
2024-11-15 17:51:26 +0800

    first commit

---
+++ sql_data_clean/test/db2/000001-张三-123456-demo.sql
@@ -0,0 +1,1 @@
+delete from test_1 where id = 1;
---
+++ sql_feature/db2/000001-张三-123456-demo.sql
@@ -0,0 +1,3 @@
+CREATE TABLE `test_1` (`aaaa` varchar(50)
+    CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci)
+    ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
